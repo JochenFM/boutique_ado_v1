96ce6b8934013a3d66784905b90ec575d7e9d88d
JochenFM <jochen_mayer@gmx.de>
2021-04-27 13:44:15 +0000

    added add to bag functionality

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+
 ]
