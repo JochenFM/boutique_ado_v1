69b2cf3aa069120065010238113ea26f7bdb125e
JochenFM <jochen_mayer@gmx.de>
2021-05-18 09:41:44 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IsOoRLXVuGFbEGl1ayutspp6yJJ1MJoy0xf3o7ft1A6JCAGrcbRilbrZiPFd3xGSiJJA5gzlelLIsI7htg13U1U00VbAQsfv6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
